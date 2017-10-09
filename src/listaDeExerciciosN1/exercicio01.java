ebb412d9014a149bca3447061e0fcb18c37f79d1
Luciwell <sis__7@hotmail.com>
2017-10-09 16:55:24 -0300

    corrigindo texto

--- src/listaDeExerciciosN1/exercicio01.java
+++ src/listaDeExerciciosN1/exercicio01.java
@@ -4,7 +4,7 @@ public class exercicio01 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("è precsiso fazer todos os exercicios");
+		System.out.println("aaaaaaa");
 		
 		
 	}
